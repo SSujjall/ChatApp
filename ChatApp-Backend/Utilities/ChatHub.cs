175b05d25c7e87669cdbc0128bceafd11205bc82
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2024-09-25 17:02:51 +0545

    initial commit

---
+++ ChatApp-Backend/Utilities/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using ChatApp_Backend.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp_Backend.Utilities
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined");
+        }
+    }
+}
